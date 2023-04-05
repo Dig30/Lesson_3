2b3b403a33dfceccbc8e2ef759b18466a66f34aa
Родин Юрий Александрович <derek.ass98@Mail.ru>
2023-04-05 18:19:33 +0300

    Task 19

---
+++ Task19/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+Console.WriteLine("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+if(number <= 99999 && number >= 10000 || number >= -99999 && number <= -10000)
+{
+    if(number / 10000 == number % 10 && number / 1000 % 10 == number / 10 % 10)
+    {
+        Console.WriteLine("Да");       
+    }
+    else
+    {
+        Console.WriteLine("Нет");  
+    }
+}
+else
+{
+    Console.WriteLine("Число не пятизначное! ");
+}
+
+
+
+
