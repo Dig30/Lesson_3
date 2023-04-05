022103c1cb2b411c90224a43860efc43f2e87417
Родин Юрий Александрович <derek.ass98@Mail.ru>
2023-04-05 19:35:21 +0300

    Task 23

---
+++ Task23/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+Console.WriteLine("Введите число N: ");
+int number = int.Parse(Console.ReadLine());
+
+int i;
+for(i = 1; i < number; i++)
+{
+    Console.Write(Math.Pow(i, 3) + ", ");
+}
+Console.WriteLine(Math.Pow(i, 3));
